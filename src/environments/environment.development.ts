c19bb7c4479058e6ecca327794bb9e4ad3322680
Faristol <95976056+Faristol@users.noreply.github.com>
2024-03-14 19:07:21 +0100

    Actualizar environment.development.ts

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-  SUPABASE_URL: 'https://iiyzradaqcqnohumerdt.supabase.co',
-  SUPABASE_KEY:
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpeXpyYWRhcWNxbm9odW1lcmR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwMjM1MzYsImV4cCI6MjAyMzU5OTUzNn0.DLVncB6BoRrphBY-6UIYTW9uLebFBC8PasaZsi8IHWQ'
-};
